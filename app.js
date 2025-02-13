d14fdd86be2c36cd3d3ba2e1c5a833545dcde5dd
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-13 15:09:51 +0000

    lecture-28: closing event loop.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req);
+    // process.exit() :- incase you want to exit the process and close the event loop
 })
 
 server.listen(3000);
