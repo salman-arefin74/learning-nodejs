baea93b8acf745ecf6c638988b2efed96f350ad1
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-13 15:21:53 +0000

    lecture-31: sending responses

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req);
     // process.exit() :- incase you want to exit the process and close the event loop
+
+    // sending response:
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My server </title></head>');
+    res.write('<body>Hello from my NodeJS server!</body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3000);
