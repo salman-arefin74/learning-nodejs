48316c9cf1b15ba172adc0391ebbe4c3c3787714
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-15 17:14:22 +0000

    lecture-36: understanding code execution

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
             console.log(body);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             console.log(message);
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end(); 
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end(); 
     }
     // sending response:
     res.setHeader('Content-Type', 'text/html');
