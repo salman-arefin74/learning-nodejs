47422e993d9c6b017d2278c42eee6473101cef9d
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-15 17:46:37 +0000

    lecture-39: nodejs module system.

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    //console.log(req);
-    // process.exit() :- incase you want to exit the process and close the event loop
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title> Message </title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();    
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-            console.log(body);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            console.log(message);
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end(); 
-            });
-        });
-    }
-    // sending response:
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> My server </title></head>');
-    res.write('<body>Hello from my NodeJS server!</body>');
-    res.write('</html>');
-    res.end();
-})
-
+const server = http.createServer(routes);
 server.listen(3000);
