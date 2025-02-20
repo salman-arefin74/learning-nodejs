a4a0338729c13ec48253b8f9979a24376ff63a07
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-20 14:27:57 +0000

    lecture-61: adding middleware

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(`Request method: ${req.method}`);
+    next();
+});
+app.use((req, res, next) => {
+    console.log(`Request method: ${req.method}`);
+});
+
 const server = http.createServer(app);
 server.listen(9001);
