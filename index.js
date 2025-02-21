78e55bf1fc4542ada5f94a6aa9f041d8756eb76a
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-21 22:43:59 +0600

    lecture-65: parsing incoming requests

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    console.log(`First middleware!`);
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send('<body><form action="product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form></body>');
 });
-app.use((req, res, next) => {
-    console.log(`Second middleware!`);
-    res.send('<h1>Hello from express.js</h1>');
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>This is the front page!!</h1>');
 });
 
 app.listen(9001);
