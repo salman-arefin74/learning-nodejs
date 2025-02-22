9f50f7f1bb49c4899f88527bb8b6b07729afd1c1
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-22 17:10:36 +0000

    lecture-67: using express router

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<body><form action="product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form></body>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>This is the front page!!</h1>');
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(9001);
