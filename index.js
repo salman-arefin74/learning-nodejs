2327f2beebc574b491396c7a309abb58839de04b
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-23 18:02:41 +0000

    lecture-72: returning a 404 page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +11,7 @@ app.use(adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(9001);
