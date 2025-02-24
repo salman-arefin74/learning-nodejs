2619f929193f5845d7db6bd9a5a9500a82620326
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-24 13:35:44 +0000

    lecture-76: serving static pages

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(adminRoute);
 app.use(shopRoute);
