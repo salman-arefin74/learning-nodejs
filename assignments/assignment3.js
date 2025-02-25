6a8505ed20b74de9dcdf203cb3d80f176e8c228b
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-25 18:30:40 +0600

    Finished Assignment 3

---
+++ assignments/assignment3.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use('/users', (req, res, next) => {
+    res.send('<h1>Users</h1>');
+});
+
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Home</h1>');
+});
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!</h1>'));
+});
+
+app.listen(9001);
