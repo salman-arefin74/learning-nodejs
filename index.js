ad7f8a32c01ced10297334433297ffcbe57d3ab8
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-26 12:17:18 +0000

    lecture-82: adding and implementing templating engines

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const shopRoute = require('./routes/shop');
 const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../utils/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop');
 });
 
 module.exports = router;
