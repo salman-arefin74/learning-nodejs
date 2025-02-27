65054c5ef8fb07f175cce509c760cd4bd68b8928
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-27 13:14:56 +0000

    lecture-87: finishing the pug template

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../utils/path');
 
 const router = express.Router();
 
 const products = [];
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product');
 });
 
 router.post('/add-product', (req, res, next) => {
