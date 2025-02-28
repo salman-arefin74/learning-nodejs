e4bf5a453fe12b4697b9c87183c11c3716582f2d
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-02-28 12:39:13 +0000

    lecture-91: implementing handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const products = [];
+
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', {pageTitle: 'Add Product', path: '/add-product'});
-});
+    res.render('add-product', { pageTitle: 'Add Product', path: '/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+  });
 
 router.post('/add-product', (req, res, next) => {
     products.push({title: req.body.title});
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,14 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const products = adminData.products;
-    res.render('shop', { products: products, docTitle: 'My Shop' });
-});
+    res.render('shop', {
+      products: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
 
 module.exports = router;
