2f0debfe6826cce9fcd4dc2006381052fb086d86
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-01 17:01:57 +0000

    lecture-93: working with ejs

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const path = require('path');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//   })
+// );
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
