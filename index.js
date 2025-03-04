d9d0b63e22eeba5e898946d4d4b8be2b73a1e3e4
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-04 07:06:14 +0000

    lecture-98: cleaning the index.js

--- index.js
+++ index.js
@@ -3,20 +3,8 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 const path = require('path');
-// const expressHbs = require('express-handlebars');
 
 const app = express();
-
-// app.engine(
-//   'hbs',
-//   expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   })
-// );
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
