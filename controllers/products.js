52f9520307d87d614f69579d99f811a7750cd630
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-04 07:22:28 +0000

    lecture-99: adding controllers

---
+++ controllers/products.js
@@ -0,0 +1,20 @@
+const products = [];
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', { pageTitle: 'Add Product', path: '/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    products.push({title: req.body.title});
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop', {
+      products: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+};
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminData = require('./routes/admin');
-const shopRoute = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const path = require('path');
 
 const app = express();
@@ -11,8 +11,8 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminData.routes);
-app.use(shopRoute);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
