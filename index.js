fed9c28ca4d67c71cf85ae7d90d006bd4cc621c6
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-04 07:34:42 +0000

    lecture-99: added prettier

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(9001);
