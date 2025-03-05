8d72a9085b9e6a713158c8041e1fbc2fcaab058a
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-05 15:23:46 +0000

    lecture-110: fixed error.js

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,3 @@
-const path = require('path');
 exports.get404 = (req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, '../views/other-templates', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
