3b0ad7670e75cb07c7acdc241a5c0251f3de9edd
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-05 14:06:51 +0000

    lecture-108: creating the shop structure

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
-    path: '/add-product',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
