a147db56232f55d78c37923df4a9adfdd6b516f4
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-10 16:25:52 +0000

    lecture-123: passing data with post requests

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,8 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', shopController.getCart);
-router.get('/cart', shopController.getOrders);
+router.post('/cart', shopController.postCart);
+router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
