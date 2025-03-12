d8f54b2799ff68729d46d61fdc491e2f1d4df2d2
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2025-03-12 23:51:39 +0600

    Finished Assignment 3

--- assignments/assignment3.js
+++ assignments/assignment3.js
@@ -13,7 +13,7 @@ app.use('/', (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found!</h1>'));
+    res.status(404).send('<h1>Page Not Found!!</h1>'));
 });
 
 app.listen(9001);
